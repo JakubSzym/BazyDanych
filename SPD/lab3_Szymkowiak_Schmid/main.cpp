257d2f5059e3ed08f30222320e0b45751a4af4c9
JakubSzym <kubaszym77@gmail.com>
2022-04-27 09:31:58 +0200

    Version 2

---
+++ SPD/lab3_Szymkowiak_Schmid/main.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct NEHData
+{
+  vector<int> values;
+  vector<int> ids;
+  int machines;
+  int tasks;
+};
+
+void readData(vector<NEHData> &list)
+{
+  string header;
+  int units;
+
+  fstream file;
+  file.open("Data.txt", ios::in);
+
+  //
+  // Wczytanie danych
+  //
+
+  for (int x = 0; x < 120; x++)
+  {
+    NEHData tmpData;
+    file >> header >> tmpData.tasks >> tmpData.machines;
+    for (int i = 0; i < tmpData.tasks * tmpData.machines; i++)
+    {
+      file >> units;
+      tmpData.values.push_back(units);
+    }
+    for (int i = 0; i < tmpData.tasks; i++)
+    {
+      tmpData.ids.push_back(i);
+    }
+    list.push_back(tmpData);
+  }
+
+  file.close();
+}
+
+int length(int number, NEHData &userdata)
+{
+  vector<int> help;
+
+  //
+  // Wypełnienie zerami wektora pomocniczego
+  //
+
+  for (int i = 0; i < userdata.machines; i++)
+  {
+    help.push_back(0);
+  }
+
+  //
+  // Obliczenie długości wykonywania zadań
+  //
+
+  for (int i = 0; i < number; i++)
+  {
+    for (int j = 1; j <= userdata.machines; j++)
+    {
+      help[j] = max(help[j], help[j-1]) + userdata.values[(j-1) + userdata.ids[i] * userdata.machines];
+    }
+  }
+
+  return help[userdata.machines];
+}
+
+void sortByWeight(NEHData &userdata)
+{
+  vector<int> weights;
+
+  //
+  // Obliczenie wag
+  //
+
+  for (int i = 0; i < userdata.tasks; i++)
+  {
+    int tmp = 0;
+    for (int j = 0; j < userdata.machines; j++)
+    {
+      tmp += userdata.values[i * userdata.machines + j];
+    }
+    weights.push_back(tmp);
+  }
+
+  //
+  // Sortowanie wg wag
+  //
+  for (int i = 0; i < userdata.tasks - 1; i++)
+  {
+    for (int j = 0; j < userdata.tasks - 1; j++)
+    {
+      if (weights[i] < weights[i+1])
+      {
+        swap(weights[i], weights[i+1]);
+        swap(userdata.ids[i], userdata.ids[i+1]);
+      }
+    }
+  }
+}
+
+int runNEH(NEHData &userdata)
+{
+  sortByWeight(userdata);
+
+  for (int i = 0; i < userdata.tasks; i++)
+  {
+    int optimalPosition = -1;
+    int optimalLength   = INT32_MAX;
+
+    for (int pos = i; pos >= 0; pos--)
+    {
+      int tmpLength = length(i+1, userdata);
+      if (optimalLength >= tmpLength)
+      {
+        optimalLength = tmpLength;
+        optimalPosition = pos;
+      }
+
+      if (pos != 0)
+      {
+        swap(userdata.ids[pos], userdata.ids[pos-1]);
+      }
+    }
+
+    for (int j = 0; j < optimalPosition; j++)
+    {
+      swap(userdata.ids[j], userdata.ids[j+1]);
+    }
+  }
+  return length(userdata.tasks, userdata);
+}
+
+int main()
+{
+  vector<NEHData> database;
+    
+  readData(database);
+
+  /*for (auto &item: database)
+  {
+    int result = runNEH(item);
+    cout << result << endl;
+  }*/
+
+  int res = runNEH(database[1]);
+  cout << res << endl;
+}
